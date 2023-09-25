3ed24ea8bd028ca53c857afa8f526601c82e74c0
Fecef <fecef.figueiredo@gmail.com>
2023-09-25 20:01:53 -0300

    feat: add pagination

--- _core/settings.py
+++ _core/settings.py
@@ -121,6 +121,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 4,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
